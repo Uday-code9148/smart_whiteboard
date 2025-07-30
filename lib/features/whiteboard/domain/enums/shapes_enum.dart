f6a4ab4d010ba433520571dc8ce2cc86270da3d6
1183 <udaykumar@instavc.com>
2025-07-30 13:05:31 +0530

    shapes enum added

--- lib/features/whiteboard/domain/enums/shapes_enum.dart
+++ lib/features/whiteboard/domain/enums/shapes_enum.dart
@@ -2,13 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 enum ShapesEnum {
-  line(Icon(Icons.calculate_rounded, color: Colors.yellow)),
-  rectangle(Icon(Icons.calculate_rounded, color: Colors.brown)),
-  square(Icon(Icons.calculate_rounded, color: Colors.black12)),
-  circle(Icon(Icons.calculate_rounded, color: Colors.orange)),
-  ellipse(Icon(Icons.calculate_rounded, color: Colors.red)),
-  triangle(Icon(Icons.calculate_rounded, color: Colors.greenAccent)),
-  arrow(Icon(Icons.calculate_rounded, color: Colors.blue));
+  line(Icon(Icons.line_axis, color: Colors.yellow)),
+  rectangle(Icon(Icons.rectangle_outlined, color: Colors.brown)),
+  square(Icon(Icons.square_rounded, color: Colors.black12)),
+  circle(Icon(Icons.circle_outlined, color: Colors.orange)),
+  ellipse(Icon(Icons.hexagon, color: Colors.red)),
+  triangle(Icon(Icons.terrain_outlined, color: Colors.greenAccent)),
+  arrow(Icon(Icons.arrow_back, color: Colors.blue));
 
   final Icon icon;
 

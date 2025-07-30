a121a2127c8fc191b3b5b6b726c871576ec8a5f3
1183 <udaykumar@instavc.com>
2025-07-30 11:59:44 +0530

    shapes added according implementation added for paint

--- lib/features/whiteboard/domain/entities/drawing_point_entity.dart
+++ lib/features/whiteboard/domain/entities/drawing_point_entity.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:inboard_personal_project/core/wrappers/icon_with_list_wrapper.dart';
+import 'package:inboard_personal_project/features/whiteboard/domain/enums/category_enum.dart';
+import 'package:inboard_personal_project/features/whiteboard/domain/enums/shapes_enum.dart';
 
 class DrawingPointEntity<T> {
   final List<Offset> points;
   final Paint? paint;
+  final ShapesEnum? shape;
+  final CategoryEnum? categoryEnum;
 
   final RecognizedWrapper<T>? recDrawing;
 
   final FeatureCategory? selectedCategory;
   final Rect? position;
 
-  DrawingPointEntity({this.points = const [], this.paint, this.recDrawing, this.selectedCategory, this.position});
+  DrawingPointEntity({
+    this.points = const [],
+    this.shape,
+    this.categoryEnum = CategoryEnum.pen,
+    this.paint,
+    this.recDrawing,
+    this.selectedCategory,
+    this.position,
+  });
 
   DrawingPointEntity copyWith({
     List<Offset>? points,
